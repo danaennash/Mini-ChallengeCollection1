601916b2a35f6d12ad262586e0ec09239f4631ba
danaennash <36461588+danaennash@users.noreply.github.com>
2019-09-04 20:59:26 -0500

    Add files via upload

---
+++ Functions.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+func check (value: Int) -> String? {
+    
+    if (value <= 0){
+        return nil
+    } else {
+        return String(value)
+    }
+}
+
+let numberString1: String? = check(value: 10) 
+let numberString2: String? = check(value: 0)
+let numberString3: String? = check(value: -2)
